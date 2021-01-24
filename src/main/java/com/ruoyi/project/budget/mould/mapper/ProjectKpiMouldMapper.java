a75e8d59aae86f0ff9e86f2c6b0d5d5885fbd5b7
yanghaichuan <51939690+yanghaichuan@users.noreply.github.com>
2021-01-24 22:58:19 +0800

    绩效模板

---
+++ src/main/java/com/ruoyi/project/budget/mould/mapper/ProjectKpiMouldMapper.java
@@ -0,0 +1,61 @@
+package com.ruoyi.project.budget.mould.mapper;
+
+import java.util.List;
+import com.ruoyi.project.budget.mould.domain.ProjectKpiMould;
+
+/**
+ * 项目绩效目标Mapper接口
+ * 
+ * @author yueqiangu
+ * @date 2021-01-24
+ */
+public interface ProjectKpiMouldMapper 
+{
+    /**
+     * 查询项目绩效目标
+     * 
+     * @param id 项目绩效目标ID
+     * @return 项目绩效目标
+     */
+    public ProjectKpiMould selectProjectKpiMouldById(Long id);
+
+    /**
+     * 查询项目绩效目标列表
+     * 
+     * @param projectKpiMould 项目绩效目标
+     * @return 项目绩效目标集合
+     */
+    public List<ProjectKpiMould> selectProjectKpiMouldList(ProjectKpiMould projectKpiMould);
+
+    /**
+     * 新增项目绩效目标
+     * 
+     * @param projectKpiMould 项目绩效目标
+     * @return 结果
+     */
+    public int insertProjectKpiMould(ProjectKpiMould projectKpiMould);
+
+    /**
+     * 修改项目绩效目标
+     * 
+     * @param projectKpiMould 项目绩效目标
+     * @return 结果
+     */
+    public int updateProjectKpiMould(ProjectKpiMould projectKpiMould);
+
+    /**
+     * 删除项目绩效目标
+     * 
+     * @param id 项目绩效目标ID
+     * @return 结果
+     */
+    public int deleteProjectKpiMouldById(Long id);
+
+    /**
+     * 批量删除项目绩效目标
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+    public int deleteProjectKpiMouldByIds(String[] ids);
+}
