93ea456708bfbab6a93b2dd2e11b36067f741e92
yanghaichuan <51939690+yanghaichuan@users.noreply.github.com>
2021-01-26 00:17:15 +0800

    绩效目标

---
+++ src/main/java/com/ruoyi/project/budget/target/controller/ProjectKpiTargetController.java
@@ -0,0 +1,126 @@
+package com.ruoyi.project.budget.target.controller;
+
+import java.util.List;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.ruoyi.framework.aspectj.lang.annotation.Log;
+import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
+import com.ruoyi.project.budget.target.domain.ProjectKpiTarget;
+import com.ruoyi.project.budget.target.service.IProjectKpiTargetService;
+import com.ruoyi.framework.web.controller.BaseController;
+import com.ruoyi.framework.web.domain.AjaxResult;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.framework.web.page.TableDataInfo;
+
+/**
+ * 项目绩效模板Controller
+ * 
+ * @author yueqiangu
+ * @date 2021-01-25
+ */
+@Controller
+@RequestMapping("/budget/target")
+public class ProjectKpiTargetController extends BaseController
+{
+    private String prefix = "budget/target";
+
+    @Autowired
+    private IProjectKpiTargetService projectKpiTargetService;
+
+    @RequiresPermissions("budget:target:view")
+    @GetMapping()
+    public String target()
+    {
+        return prefix + "/target";
+    }
+
+    /**
+     * 查询项目绩效模板列表
+     */
+    @RequiresPermissions("budget:target:list")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(ProjectKpiTarget projectKpiTarget)
+    {
+        startPage();
+        List<ProjectKpiTarget> list = projectKpiTargetService.selectProjectKpiTargetList(projectKpiTarget);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出项目绩效模板列表
+     */
+    @RequiresPermissions("budget:target:export")
+    @Log(title = "项目绩效模板", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(ProjectKpiTarget projectKpiTarget)
+    {
+        List<ProjectKpiTarget> list = projectKpiTargetService.selectProjectKpiTargetList(projectKpiTarget);
+        ExcelUtil<ProjectKpiTarget> util = new ExcelUtil<ProjectKpiTarget>(ProjectKpiTarget.class);
+        return util.exportExcel(list, "target");
+    }
+
+    /**
+     * 新增项目绩效模板
+     */
+    @GetMapping("/add")
+    public String add()
+    {
+        return prefix + "/add";
+    }
+
+    /**
+     * 新增保存项目绩效模板
+     */
+    @RequiresPermissions("budget:target:add")
+    @Log(title = "项目绩效模板", businessType = BusinessType.INSERT)
+    @PostMapping("/add")
+    @ResponseBody
+    public AjaxResult addSave(ProjectKpiTarget projectKpiTarget)
+    {
+        return toAjax(projectKpiTargetService.insertProjectKpiTarget(projectKpiTarget));
+    }
+
+    /**
+     * 修改项目绩效模板
+     */
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Long id, ModelMap mmap)
+    {
+        ProjectKpiTarget projectKpiTarget = projectKpiTargetService.selectProjectKpiTargetById(id);
+        mmap.put("projectKpiTarget", projectKpiTarget);
+        return prefix + "/edit";
+    }
+
+    /**
+     * 修改保存项目绩效模板
+     */
+    @RequiresPermissions("budget:target:edit")
+    @Log(title = "项目绩效模板", businessType = BusinessType.UPDATE)
+    @PostMapping("/edit")
+    @ResponseBody
+    public AjaxResult editSave(ProjectKpiTarget projectKpiTarget)
+    {
+        return toAjax(projectKpiTargetService.updateProjectKpiTarget(projectKpiTarget));
+    }
+
+    /**
+     * 删除项目绩效模板
+     */
+    @RequiresPermissions("budget:target:remove")
+    @Log(title = "项目绩效模板", businessType = BusinessType.DELETE)
+    @PostMapping( "/remove")
+    @ResponseBody
+    public AjaxResult remove(String ids)
+    {
+        return toAjax(projectKpiTargetService.deleteProjectKpiTargetByIds(ids));
+    }
+}
---
+++ src/main/java/com/ruoyi/project/budget/target/domain/ProjectKpiTarget.java
@@ -0,0 +1,194 @@
+package com.ruoyi.project.budget.target.domain;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import com.ruoyi.framework.aspectj.lang.annotation.Excel;
+import com.ruoyi.framework.web.domain.BaseEntity;
+
+/**
+ * 项目绩效模板对象 project_kpi_target
+ * 
+ * @author yueqiangu
+ * @date 2021-01-25
+ */
+public class ProjectKpiTarget extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** ID */
+    private Long id;
+
+    /** 项目ID */
+    private Long projectId;
+
+    /** 项目名称 */
+    @Excel(name = "项目名称")
+    private String projectName;
+
+    /** 一级指标ID */
+    @Excel(name = "一级指标ID")
+    private Long oneLevel;
+
+    /** 一级指标 */
+    @Excel(name = "一级指标")
+    private String oneLevelName;
+
+    /** 二级指标ID */
+    @Excel(name = "二级指标ID")
+    private Long twoLevel;
+
+    /** 二级指标 */
+    @Excel(name = "二级指标")
+    private String twoLevelName;
+
+    /** 三级指标ID */
+    @Excel(name = "三级指标ID")
+    private Long threeLevel;
+
+    /** 三级指标 */
+    @Excel(name = "三级指标")
+    private String threeLevelName;
+
+    /** 指标内容 */
+    @Excel(name = "指标内容")
+    private String target;
+
+    /** 完成率 */
+    @Excel(name = "完成率")
+    private String targetRate;
+
+    /** 删除标志（0代表存在 1代表删除） */
+    private String delFlag;
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public Long getId()
+    {
+        return id;
+    }
+    public void setProjectId(Long projectId)
+    {
+        this.projectId = projectId;
+    }
+
+    public Long getProjectId()
+    {
+        return projectId;
+    }
+    public void setProjectName(String projectName)
+    {
+        this.projectName = projectName;
+    }
+
+    public String getProjectName()
+    {
+        return projectName;
+    }
+    public void setOneLevel(Long oneLevel)
+    {
+        this.oneLevel = oneLevel;
+    }
+
+    public Long getOneLevel()
+    {
+        return oneLevel;
+    }
+    public void setOneLevelName(String oneLevelName)
+    {
+        this.oneLevelName = oneLevelName;
+    }
+
+    public String getOneLevelName()
+    {
+        return oneLevelName;
+    }
+    public void setTwoLevel(Long twoLevel)
+    {
+        this.twoLevel = twoLevel;
+    }
+
+    public Long getTwoLevel()
+    {
+        return twoLevel;
+    }
+    public void setTwoLevelName(String twoLevelName)
+    {
+        this.twoLevelName = twoLevelName;
+    }
+
+    public String getTwoLevelName()
+    {
+        return twoLevelName;
+    }
+    public void setThreeLevel(Long threeLevel)
+    {
+        this.threeLevel = threeLevel;
+    }
+
+    public Long getThreeLevel()
+    {
+        return threeLevel;
+    }
+    public void setThreeLevelName(String threeLevelName)
+    {
+        this.threeLevelName = threeLevelName;
+    }
+
+    public String getThreeLevelName()
+    {
+        return threeLevelName;
+    }
+    public void setTarget(String target)
+    {
+        this.target = target;
+    }
+
+    public String getTarget()
+    {
+        return target;
+    }
+    public void setTargetRate(String targetRate)
+    {
+        this.targetRate = targetRate;
+    }
+
+    public String getTargetRate()
+    {
+        return targetRate;
+    }
+    public void setDelFlag(String delFlag)
+    {
+        this.delFlag = delFlag;
+    }
+
+    public String getDelFlag()
+    {
+        return delFlag;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("id", getId())
+            .append("projectId", getProjectId())
+            .append("projectName", getProjectName())
+            .append("oneLevel", getOneLevel())
+            .append("oneLevelName", getOneLevelName())
+            .append("twoLevel", getTwoLevel())
+            .append("twoLevelName", getTwoLevelName())
+            .append("threeLevel", getThreeLevel())
+            .append("threeLevelName", getThreeLevelName())
+            .append("target", getTarget())
+            .append("targetRate", getTargetRate())
+            .append("createBy", getCreateBy())
+            .append("createTime", getCreateTime())
+            .append("updateBy", getUpdateBy())
+            .append("updateTime", getUpdateTime())
+            .append("remark", getRemark())
+            .append("delFlag", getDelFlag())
+            .toString();
+    }
+}
